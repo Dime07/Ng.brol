9e2f25dc34105ac3f3f818faf26577c3e4da15ef
Taufikisme <mohtaufikafandi@gmail.com>
2021-09-03 18:22:14 +0700

    Edit Home Page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,10 @@
-import Navbar from "../Components/Navbar"
-import Hero from "../Components/Hero"
+import Navbar from "../Components/Navbar";
+import Hero from "../Components/Hero";
 export default function Home() {
   return (
     <div>
       <Navbar />
       <Hero />
     </div>
-  )
+  );
 }
