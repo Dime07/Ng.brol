8c2560b481a03cc2d762e2db9357a69b786c48f1
Dime07 <dimasbkt7@gmail.com>
2021-09-19 13:39:14 +0700

    menambahkan halaman messages dan sidebar

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 const Home = React.lazy(() => import("./pages/Home"));
+const Messages = React.lazy(() => import("./pages/Messages"))
 
 export default function App() {
   return (
@@ -9,6 +10,9 @@ export default function App() {
       <Router>
         <Switch>
           <Route exact path={`/`} component={Home} />
+        </Switch>
+        <Switch>
+          <Route path={'/messages'} component={Messages} />
         </Switch>
       </Router>
     </React.Suspense>
---
+++ src/pages/Messages.js
@@ -0,0 +1,8 @@
+import Sidebar from "../Components/Sidebar"
+export default function Messages(){
+    return(
+        <div className="messages bg-main-color h-screen text-white">
+            <Sidebar />
+        </div>
+    )
+}
